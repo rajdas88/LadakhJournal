9850339008aa2607c0b076524bcfdd5d92f7715d
Rajarshi Das <rajdas88@gmail.com>
2013-06-27 16:46:09 +0100

    Added list functionality and label on hover code
    List is using controls, probably better to just have it as separate
    elements that link to the markers. Would like them to be drag, drop and
    lock-able. The hover was included to avoid popups, but can't add
    separate click and double click events on the markers. May be useful for
    later iteration though...

--- js/myMap.js
+++ js/myMap.js
@@ -7,11 +7,12 @@ L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/
 	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
 }).addTo(map);
 
+//Change name from leaficon
 var LeafIcon = L.Icon.extend({
 	options: {
 	shadowUrl: 'images/marker-shadow.png',
-	iconAnchor:   [12, 41],
-	shadowAnchor: [12, 41],
+	iconAnchor:   [12, 20], // changed 41 to 20 for label placement
+	shadowAnchor: [12, 20], // same as above 
 	popupAnchor:  [0, -34]
 	}
 });
@@ -19,12 +20,19 @@ var LeafIcon = L.Icon.extend({
 var blueIcon = new LeafIcon({iconUrl: 'images/marker-icon-blue.png'}),
 	redIcon = new LeafIcon({iconUrl: 'images/marker-icon-red.png'});
 
-var link;
+// Generalize for more than 2 categories
+var link, tpdiv, div;
 var cat1 = [];
 var cat2 = [];
-var marker
+var marker;
+var markerList = {};
+var myList = {};
+var addFlag;
+//alert($.inArray(4, myList));
+// Consider using leaflet labels instead of popup?
+//https://github.com/Leaflet/Leaflet.label
 for (var i = 0; i < LadakhPlaces.length; i++) {
-	link = "<a href='#' class='speciallink'>" + LadakhPlaces[i].Name + "</a>"
+	//link = "<a href='#' class='speciallink'>" + LadakhPlaces[i].Name + "</a>"
 	link = $("<a href='#'>" + LadakhPlaces[i].Name + "</a>").on('click', {name: LadakhPlaces[i].Name, desc: LadakhPlaces[i].Description, image: LadakhPlaces[i].Image}, function(event) {
 		Lightview.show({
 			url: event.data.image,
@@ -34,20 +42,71 @@ for (var i = 0; i < LadakhPlaces.length; i++) {
 				width: 600
 			}
 			});
+	});
+	addFlag = $.inArray(i, myList) > 0 ? "Remove" : "Add"
+	tpdiv = $("<a href='#'>" + "Add/Remove" + "</a>").on('click', {name: LadakhPlaces[i].Name, number: i}, function(event) {
+		if (!(event.data.number in myList)) {
+		
+			var scheduleItem = L.control();
+
+			scheduleItem.onAdd = function (map) {
+				this._div = L.DomUtil.create('div', 'item'); // create a div with a class "item"
+				this.update();
+				return this._div;
+			};
+
+			// method that we will use to update the control based on feature properties passed
+			scheduleItem.update = function (props) {
+				this._div.innerHTML = event.data.name;
+			};
+
+			map.addControl(scheduleItem);
+			myList[event.data.number] = scheduleItem;
+			//itemList.push(scheduleItem);
+			
+			//refresh marker add -> remove
+			//map.removeLayer(markerList[i]);
+			//delete markerList[i];
+			//var new_marker = L.marker([LadakhPlaces[i].Lat, LadakhPlaces[i].Long], {icon: blueIcon, opacity: 0.8, riseOnHover: true}).bindPopup(div);
+			//markerList[i] = new_marker;
+			//map.addLayer(markerList[i]);
+		} else {
+			map.removeControl(myList[event.data.number]);
+			delete myList[event.data.number];
+		}		
 	})[0];
-	if (LadakhPlaces[i].Lat != "" && i%2==0) {
-		marker = L.marker([LadakhPlaces[i].Lat, LadakhPlaces[i].Long], {icon: blueIcon, opacity: 0.8, riseOnHover: true}).bindPopup(link);
-		cat1.push(marker)
+	div = $('<div />').append(link).append("<br>").append(tpdiv)[0];
+	if (LadakhPlaces[i].Lat != "" && i%2 == 0) {
+		marker = L.marker([LadakhPlaces[i].Lat, LadakhPlaces[i].Long], {icon: blueIcon, opacity: 0.8, riseOnHover: true}).bindPopup(div);
+		//.bindLabel('Look revealing label!').addTo(map);
+		/*marker = L.marker([LadakhPlaces[i].Lat, LadakhPlaces[i].Long], {icon: blueIcon, opacity: 0.8, riseOnHover: true}).
+		on('click', function() {
+			Lightview.show({
+				url: this.image,
+				title: this.name,
+				caption: this.desc,
+				options: {
+					width: 600
+				}
+			});
+		}, {name: LadakhPlaces[i].Name, desc: LadakhPlaces[i].Description, image: LadakhPlaces[i].Image}).
+		bindLabel(LadakhPlaces[i].Name);*/
+		
+		//marker.on('dblclick', function() {
+		//alert('dbl');}
+		cat1.push(marker);
+		markerList[i] = marker;
 		map.addLayer(marker);
 		//L.marker([LadakhPlaces[i].Lat, LadakhPlaces[i].Long], {icon: blueIcon}).addTo(map).bindPopup(link);
 	} else {
-		marker = L.marker([LadakhPlaces[i].Lat, LadakhPlaces[i].Long], {icon: redIcon, opacity: 0.8, riseOnHover: true}).bindPopup(link);
-		cat2.push(marker)
+		marker = L.marker([LadakhPlaces[i].Lat, LadakhPlaces[i].Long], {icon: redIcon, opacity: 0.8, riseOnHover: true}).bindPopup(div);
+		cat2.push(marker);
+		markerList[i] = marker;
 		map.addLayer(marker);
 	}
 }
 
-var legend = L.control({position: 'bottomright'});
+var legend = L.control({position: 'bottomleft'});
 
 legend.onAdd = function (map) {
 
@@ -56,8 +115,8 @@ legend.onAdd = function (map) {
 		labels = [];
 
 	div.innerHTML +=
-		'<i style="background: url(images/marker-icon-blue.png)" id="category1"></i> ' + 'Lived' + '<br>' +
-		'<i style="background: url(images/marker-icon-red.png)" id="category2"></i> ' + 'Visited' + '<br>' +
+		'<div style="display: inline;" id="category1"><i style="background: url(images/marker-icon-blue.png)"></i>Lived</div><br />' +
+		'<div style="display: inline;" id="category2"><i style="background: url(images/marker-icon-red.png)"></i>Visited</div><br />' +
 		'<b id="allcategory">ALL</b>';
 
     return div;
